6c1ccfe5c331d078e4d69d969f04b271d28455b7
Subash K <81848034+subash-srk@users.noreply.github.com>
2024-03-01 19:14:36 +0530

    Add files via upload

---
+++ day_1.py
@@ -0,0 +1,13 @@
+####print
+print("Hello, World!")
+print('She said: "Hello" and left')
+print("Hello\nHello")
+print("Hello" + " " + "Subash")
+
+####input
+input("what is your name?")
+print("Hello " + input("What is your name?"))
+
+####variables
+name = input("What is your name? ")
+print(name)
