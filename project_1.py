5e0211d416d7f0576964021ed41870d0de05dd9a
Subash K <81848034+subash-srk@users.noreply.github.com>
2024-03-02 19:21:31 +0530

    Delete project_1.py

--- project_1.py
+++
@@ -1,6 +0,0 @@
-#Band name generator
-print("Welcome to the band name generator")
-city = input("Enter the city you grew up in:\n")
-pet_name = input("Enter the name of your pet:\n")
-band = city + " " + pet_name
-print("Name of Your band will be" + " " + band)
