ac7269c92f0ec39430a2d837b02cd47725befd42
Subash K <81848034+subash-srk@users.noreply.github.com>
2024-03-05 19:05:03 +0530

    Add files via upload

---
+++ day_4.py
@@ -0,0 +1,23 @@
+#Randomization
+import random
+
+random_integer  = random.randint(1,10)
+#print(random_integer)
+
+random_float = random.random()
+#print(random_float)
+
+#List
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+
+#print(states_of_america)
+
+
+dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"]
+
+fruit = ["Apple", "Orange", "Mango"]
+print(fruit[0])
+print(fruit[-1])
+
+fruit.append("Strawberry")
+print(fruit)
