c48d94e9d4759e6d0caa72752ae25dad6a98d923
Subash K <81848034+subash-srk@users.noreply.github.com>
2024-03-06 19:21:53 +0530

    Add files via upload

---
+++ day_5.py
@@ -0,0 +1,11 @@
+#For Loop
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit)
+    print(fruit +" Pie")
+
+
+total = 0
+for number in range(1,101):
+    total += number
+print(total)
