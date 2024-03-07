f351030510004e63c87f81f944727a30a8a9d288
Subash K <81848034+subash-srk@users.noreply.github.com>
2024-03-07 18:31:12 +0530

    Add files via upload

---
+++ Projects/project_6.py
@@ -0,0 +1,24 @@
+#REBORG'S WORLD MAZE SOLUTION:
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+while front_is_clear():
+    move()
+turn_left()
+    
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
+    
+
+    
+
+
