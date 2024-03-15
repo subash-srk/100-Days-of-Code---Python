e2218cbf240aeac4cbe8e92d8ee2eceac5cc33aa
Subash K <81848034+subash-srk@users.noreply.github.com>
2024-03-15 11:34:16 +0530

    Add files via upload

---
+++ dict_inList.py
@@ -0,0 +1,29 @@
+country = input() # Add country name
+visits = int(input()) # Number of visits
+list_of_cities = eval(input()) # create list from formatted string
+
+travel_log = [
+  {
+    "country": "France",
+    "visits": 12,
+    "cities": ["Paris", "Lille", "Dijon"]
+  },
+  {
+    "country": "Germany",
+    "visits": 5,
+    "cities": ["Berlin", "Hamburg", "Stuttgart"]
+  },
+]
+
+def add_new_country(name, time_visits, list_of_cities):
+  new_country = {}
+  new_country["country"] = name
+  new_country["visits"] = time_visits
+  new_country["cities"] = list_of_cities
+  travel_log.append(new_country)
+
+
+
+add_new_country(country, visits, list_of_cities)
+print(f"I've been to {travel_log[2]['country']} {travel_log[2]['visits']} times.")
+print(f"My favourite city was {travel_log[2]['cities'][0]}.")
