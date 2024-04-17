a06a3a18b27a395a1dfcf48d4f2f7dca072594d1
Subash K <81848034+subash-srk@users.noreply.github.com>
2024-04-17 19:14:11 +0530

    Add files via upload

---
+++ Projects/Project_36/Stock Trading News Alert/main1.py
@@ -0,0 +1,62 @@
+# STOCK TRADING NEWS ALERT PROJECT
+import requests
+from twilio.rest import Client
+
+TWILIO_SID = ""
+TWILIO_AUTH_TOKEN = ""
+
+STOCK_NAME = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+
+STOCK_API_KEY = ""
+NEWS_API_KEY = ""
+
+stock_params = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK_NAME,
+    "apikey": STOCK_API_KEY
+}
+
+response = requests.get(STOCK_ENDPOINT, params=stock_params)
+
+data = response.json()['Time Series (Daily)']
+data_list = [value for (key, value) in data.items()]
+yesterday_data = data_list[0]
+yesterday_closing_price = yesterday_data['4. close']
+
+day_before_yesterday_data = data_list[1]
+day_before_yesterday_closing_price = day_before_yesterday_data['4. close']
+
+difference = float(yesterday_closing_price) - float(day_before_yesterday_closing_price)
+up_down = None
+if difference > 0:
+    up_down = "🔺"
+else:
+    up_down = "🔻"
+
+
+diff_percent = round(abs((difference / float(yesterday_closing_price)) * 100))
+print(diff_percent)
+
+if diff_percent > 2:
+    news_params = {
+        "apiKey": NEWS_API_KEY,
+        "qInTitle": COMPANY_NAME,
+    }
+    news_response = requests.get(NEWS_ENDPOINT, params=news_params)
+    articles = news_response.json()["articles"]
+    three_articles = articles[:3]
+
+    formatted_articles = [f"{STOCK_NAME}: {up_down}{diff_percent}%\nHeadline: {article['title']}. \nBrief: {article['description']}" for article in
+                          three_articles]
+    for article in formatted_articles:
+        client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+        message = client.messages.create(
+            body=article,
+            from_='',
+            to=''
+        )
+        print(message.status)
